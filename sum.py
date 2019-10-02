467ac75d6bb2d68c63eefc662ff4bc4f741ceca1
Govind raj <54857279+gov466@users.noreply.github.com>
2019-10-02 11:47:02 -0400

    Create sum.py

---
+++ sum.py
@@ -0,0 +1,6 @@
+'''python program to add two numbers '''
+
+a =input("Enter number a: ")    ''' input number from user'''
+b=input("Enter  number b: ")   
+c =int(a) +int(b)           ''' adding both numbers'''
+print ("sum of a and b is :{0}".format(c))  '''displaying the sum'''

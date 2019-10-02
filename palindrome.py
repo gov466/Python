3735eea66f82d4cd18993e9106d21c81685f4097
Govind raj <54857279+gov466@users.noreply.github.com>
2019-10-02 11:05:45 -0400

    Create palindrome.py

---
+++ palindrome.py
@@ -0,0 +1,7 @@
+'''Program to find word is palindrome or not'''
+
+a =input("Enter word: ") '''entering word by user'''
+if  a== a[ : : -1]:     '''checking orginal string by its reverse'''
+    print ("palindrome")    '''print palindrome if the condition is true''' 
+else:
+    print ("not palindrome")

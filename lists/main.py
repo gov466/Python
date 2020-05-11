3ec80fb74b58b0aeee38e8f435b6321611657f62
Govind raj <govind.mndy@gmail.com>
2020-05-11 15:34:37 -0400

    Create main.py

---
+++ lists/main.py
@@ -0,0 +1,19 @@
+##list in python
+name=["john","manu","govind"] ##list
+print(name)
+name.append("gary") #insert at end
+print(name)
+name.insert(0,"nandu") ##insert at 0th posisiton
+print(name)
+name.remove("manu") ##remove from list
+print(name)
+name.reverse()  ##reverse a list
+print(name)
+
+
+numbers=[6,2,1,4,8,0] #new list
+print(numbers)
+numbers.sort() #ascending ##sorting in ascendiong order
+print(numbers)
+numbers.reverse() #descending
+print(numbers)

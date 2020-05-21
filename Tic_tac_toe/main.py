d3787b856bb024c611d0500ed2d9c707d51297aa
Govind raj <govind.mndy@gmail.com>
2020-05-21 16:18:33 -0400

    Update main.py

--- Tic_tac_toe/main.py
+++ Tic_tac_toe/main.py
@@ -37,13 +37,13 @@ def play_game():
   
   #game has ended
   if winner =="X" or winner =="O":
-    print(winner + "won")
+    print(winner + " won ")
   else:
     print("Tie")
 def handle_turn(player):
   posistion= input("choose a posisition from 1-9: ")
   posistion= int(posistion)-1
-  board[posistion]="x"
+  board[posistion]="X"
   display_board()
 
 def check_if_game_over():
@@ -63,13 +63,13 @@ def check_if_winner():
   #chck diagnl
   diagonal_winner=check_diagonals()
   if row_winner:
-    winner= row_winner()
+    winner= row_winner
     #there was a winner
   elif column_winner:
-    winner=column_winner()
+    winner=column_winner
     #there was a winner
   elif diagonal_winner:
-    winner=diagonal_winner()
+    winner=diagonal_winner
     #there was a win
   else:
     #there was no winner
@@ -77,13 +77,69 @@ def check_if_winner():
   return
 
 def check_rows():
-  return
-def check_columns():
-  return
-def check_diagonals():
+  #set glabal variable
+  global game_still_going
+  #checking all rows
+  row_1= board[0]==board[1]==board[2] != "-"
+  row_2= board[3]==board[4]==board[5] != "-"
+  row_3= board[6]==board[7]==board[8] != "-"
+
+  if row_1 or row_2 or row_3:
+    game_still_going= False
+  #return the winner(X or O)
+
+  if row_1:
+    return board[0]
+  elif row_2:
+    return board[3]
+  elif row_3:
+    return board[6]
   return
 
-def chech_if_tie():
+def check_columns():
+
+   #set glabal variable
+  global game_still_going
+  #checking all rows
+  column_1= board[0]==board[3]==board[6] != "-"
+  column_2= board[1]==board[4]==board[7] != "-"
+  column_3= board[2]==board[5]==board[8] != "-"
+
+  if column_1 or column_2 or column_3:
+    game_still_going= False
+  #return the winner(X or O)
+
+  if column_1:
+    return board[0]
+  elif column_2:
+    return board[1]
+  elif column_3:
+    return board[2]
+  return
+
+def check_diagonals():
+
+   #set glabal variable
+  global game_still_going
+  #checking all rows
+  diagonal_1= board[0]==board[4]==board[8] != "-"
+  diagonal_2= board[6]==board[4]==board[2] != "-"
+   
+  #checking if any diagonasl are true   
+  if diagonal_1 or diagonal_2 :
+    game_still_going= False
+  #return the winner(X or O)
+
+  if diagonal_1:
+    return board[0]
+  elif diagonal_2:
+    return board[6]
+   
+
+  return
+
+
+def check_if_tie():
   return
 
 def flip_player():

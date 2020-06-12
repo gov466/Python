bce3557a5aed92bd323101b39864cdf3e6347ff2
Govind raj <govind.mndy@gmail.com>
2020-06-11 22:11:04 -0400

    Create main.py

---
+++ Tkinter/New_window/main.py
@@ -0,0 +1,22 @@
+from tkinter import  *
+from PIL import Image,ImageTk
+from _ast import Lambda
+
+root =Tk()
+root.title("Canada") 
+root.geometry("633x353")
+def open():
+    global my_img
+    top= Toplevel()
+    top.title("572 Pharmacy")
+    my_img= ImageTk.PhotoImage(Image.open("/home/govind/Downloads/instagram.png"))
+    my_lablel= Label(top, image=my_img).pack()
+    btn2 =Button(top,text="close window",command=top.destroy).pack()
+
+    
+ 
+my_img1=ImageTk.PhotoImage(Image.open("/home/govind/eclipse-workspace/Python/Tkinter/src/carousel-canada.jpg"))
+my_label1=Label(root, image=my_img1).pack()
+home =Button(text="Home", command=open).place(relx=0.5, rely=0.96, anchor="c")
+## =Button(text="572 live", command=open).place(relx=0.5, rely=0.96, anchor="c")
+mainloop()

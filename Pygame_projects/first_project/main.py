6360466225eb1e3e798a032dadd315f46ae9a3a4
gov466 <govind.mndy@gmail.com>
2020-07-14 20:30:34 -0400

    changes for py

--- Pygame_projects/first_project/main.py
+++ Pygame_projects/first_project/main.py
@@ -8,12 +8,21 @@ pygame.display.set_caption("space invader")
 icon = pygame.image.load('spaceship.png')
 pygame.display.set_icon(icon)
 
+# player
+playerImg = pygame.image.load('player.png')
+playerX = 370
+playerY = 480
+
+def player():
+    screen.blit(playerImg, (playerX, playerY))
 #game loop
 running = True
 while running:
+    screen.fill((0, 0, 0))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
     # RGB color
-    screen.fill((0, 0, 0))
+
+    player()
     pygame.display.update()

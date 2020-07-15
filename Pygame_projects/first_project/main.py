0fe71fad2f50b720d981e7f9867d57c2e21513a7
gov466 <govind.mndy@gmail.com>
2020-07-14 21:37:01 -0400

    on progress

--- Pygame_projects/first_project/main.py
+++ Pygame_projects/first_project/main.py
@@ -1,7 +1,8 @@
 import pygame
+import random
 
 # create the screen
-screen = pygame.display.set_mode((800,600))
+screen = pygame.display.set_mode((800, 600))
 
 # title and icon
 pygame.display.set_caption("space invader")
@@ -12,17 +13,61 @@ pygame.display.set_icon(icon)
 playerImg = pygame.image.load('player.png')
 playerX = 370
 playerY = 480
+playerX_change = 0
 
-def player():
-    screen.blit(playerImg, (playerX, playerY))
-#game loop
+# enemy
+enemyImg = pygame.image.load('skull.png')
+enemyX = random.randint(0, 800)
+enemyY = random.randint(50, 150)
+enemyX_change = 0.3
+enemyY_change = 40
+
+
+def player(x, y):
+    screen.blit(playerImg, (x, y))
+
+
+def enemy(x, y):
+    screen.blit(enemyImg, (x, y))
+
+
+# game loop
 running = True
 while running:
     screen.fill((0, 0, 0))
+    # playerX += 0.1
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    # RGB color
+    # if keystroke is pressed check whether its right or left
+    if event.type == pygame.KEYDOWN:
+        if event.key == pygame.K_LEFT:
+            playerX_change = -0.3
+        if event.key == pygame.K_RIGHT:
+            playerX_change = 0.3
+    if event.type == pygame.KEYUP:
+        if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+            playerX_change = 0
 
-    player()
+    # RGB color
+    playerX += playerX_change
+
+    if playerX <= 0:
+        playerX = 0
+    elif playerX >= 736:
+        playerX = 736
+    # checking for boundaries of enemy
+    enemyX += enemyX_change
+
+    if enemyX <= 0:
+        enemyX_change = 0.3
+        enemyY += enemyY_change
+    elif enemyX >= 736:
+        enemyX_change = -0.3
+        enemyY += enemyY_change
+
+
+    player(playerX, playerY)
+    enemy(enemyX, enemyY)
+
     pygame.display.update()
